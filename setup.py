745c255f1545234a2a7c69dd5dc606e29514606f
Mahima Arunkumar <88596850+mBiocoder@users.noreply.github.com>
2022-10-23 16:21:09 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bacdive_caller.py',
+    version='1.1.6',
+    packages=[''],
+    url='https://github.com/mBiocoder/Bacdiving',
+    license='MIT',
+    author='Mahima Arunkumar',
+    author_email='mahima.arunkumar.1@gmail.com',
+    description='Bacdiving accesses the Bacterial Diversity Metadatabase BacDive and provides various visualization options.'
+)

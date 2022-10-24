7f0090c1994c1d63876f477252f8ed4f8c9d7145
Mahima Arunkumar <88596850+mBiocoder@users.noreply.github.com>
2022-10-24 14:01:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,53 @@
-from setuptools import setup
+import io
+import os
+from setuptools import setup, find_packages
 
+# Package meta-data.
+NAME = 'bacdiving'
+DESCRIPTION = 'Bacdiving accesses the Bacterial Diversity Metadatabase BacDive and provides various visualization options.'
+URL = 'https://github.com/mBiocoder/Bacdiving'
+EMAIL = 'M.Arunkumar@campus.lmu.de'
+AUTHOR = 'Mahima Arunkumar'
+REQUIRES_PYTHON = '>=3.8.0'
+VERSION = "1.2.3"
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'alive_progress>=2.4.1',
+    'anytree>=2.8.0',
+    'bacdive>=0.2',
+    'bokeh>=2.4.3',
+    'ete3>=3.1.2',
+    'matplotlib==3.6.0',
+    'numpy>=1.23.0',
+    'pandas>=1.5.0',
+    'seaborn',
+    'setuptools>=65.5.0',
+    'scipy>=1.9.2',
+    'toyplot>=1.0.3',
+    'toytree>=2.0.1',
+    'wheel>=0.34.1',
+    'worldmap>=0.1.6']
+
+
+
+long_description = 'README.md'
+
+
+
+# Where the magic happens:
 setup(
-    name='bacdive_caller.py',
-    version='1.1.6',
-    packages=[''],
-    url='https://github.com/mBiocoder/Bacdiving',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    install_requires=REQUIRED,
+    include_package_data=True,
     license='MIT',
-    author='Mahima Arunkumar',
-    author_email='mahima.arunkumar.1@gmail.com',
-    description='Bacdiving accesses the Bacterial Diversity Metadatabase BacDive and provides various visualization options.'
+    keywords=["BacDive", "bacteria", "phenotype information"]
 )
